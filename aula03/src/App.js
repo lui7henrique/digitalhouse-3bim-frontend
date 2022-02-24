6758d17030f79f3ad6aafe1a0e702a2a84bef8e4
Luiz Henrique <7henrique18@gmail.com>
2022-02-24 15:10:01 -0300

    :tada: Timer

--- aula03/src/App.js
+++ aula03/src/App.js
@@ -1,11 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const SECOND = 1000; // 1 second in milliseconds
 
 function App() {
   const [count, setCount] = useState(0);
+  const [time, setTime] = useState({
+    hour: String(new Date().getHours()).padStart(2, "0"),
+    seconds: String(new Date().getSeconds()).padStart(2, "0"),
+    minutes: String(new Date().getMinutes()).padStart(2, "0"),
+  });
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTime({
+        hour: String(new Date().getHours()).padStart(2, "0"),
+        seconds: String(new Date().getSeconds()).padStart(2, "0"),
+        minutes: String(new Date().getMinutes()).padStart(2, "0"),
+      });
+    }, SECOND);
+  }, [time]);
 
   return (
     <div>
-      <h1>Hello, world!</h1>
+      <h1>
+        Horário atual: {time.hour}:{time.minutes}:{time.seconds}
+      </h1>
       <h3>This a Create React App! 👋</h3>
       <p>You clicked {count} times</p>
       <button onClick={() => setCount((count) => count + 1)}>Click me</button>
